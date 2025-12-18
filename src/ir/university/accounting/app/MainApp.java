2bca153d1f1d8746c6651c11f9cb3f64ff0dd973
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 03:36:20 -0800

    add contractoraccountingsystem3

--- src/ir/university/accounting/app/MainApp.java
+++ src/ir/university/accounting/app/MainApp.java
@@ -85,7 +85,11 @@ public class MainApp {
                     paymentDAO.addPayment(pid,amt,date);
                 }
 
-              }
+                case 4->{
+                    System.out.print("Project ID: ");
+                    int pid = scanner.nextInt();
+                    reportDAO.printReport(pid);
+                }
 
 
             }
