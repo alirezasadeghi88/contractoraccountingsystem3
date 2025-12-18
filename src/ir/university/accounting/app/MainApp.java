26ccc4bffca12054ce56230a9d23ac8a68673afc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 03:34:35 -0800

    add contractoraccountingsystem3

---
+++ src/ir/university/accounting/app/MainApp.java
@@ -0,0 +1,35 @@
+package ir.university.accounting.app;
+
+import ir.university.accounting.dao.ExpenseDAO;
+import ir.university.accounting.dao.PaymentDAO;
+import ir.university.accounting.dao.ProjectDAO;
+import ir.university.accounting.dao.ReportDAO;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ProjectDAO projectDAO = new ProjectDAO();
+        ExpenseDAO expenseDAO = new ExpenseDAO();
+        PaymentDAO paymentDAO = new PaymentDAO();
+        ReportDAO reportDAO = new ReportDAO();
+
+
+
+
+
+
+
+
+
+
+              }
+
+
+            }
+        }
+    }
+}
