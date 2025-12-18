b2bbe3f9a24c2e3039be88cb82eeab37166668af
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 02:38:03 -0800

    add contractoraccountingsystem3

--- src/ir/university/accounting/dao/PaymentDAO.java
+++ src/ir/university/accounting/dao/PaymentDAO.java
@@ -1,10 +1,30 @@
 package ir.university.accounting.dao;
 
+import ir.university.accounting.util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+
 public class PaymentDAO {
     public void addPayment(int projectId, double amount, String date) {
 
         String sql = " INSERT INTO payments" +
                 "(project_id, amount, payment_date)" +
                 " VALUES (?, ?, ?)";
+
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setInt(1,projectId);
+            preparedStatement.setDouble(2,amount);
+            preparedStatement.setDate(3, Date.valueOf(date));
+
+            preparedStatement.executeUpdate();
+            System.out.println("Payment added✅");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
