55029fd01b28095b08798a508a0accc38848193e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 00:27:15 -0800

    add contractoraccountingsystem3

--- src/ir/university/accounting/util/DBConnection.java
+++ src/ir/university/accounting/util/DBConnection.java
@@ -1,6 +1,8 @@
 package ir.university.accounting.util;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 public class DBConnection {
@@ -8,5 +10,28 @@ public class DBConnection {
     private static String user;
     private static String password;
 
+    static {
+        try {
+            Properties properties = new Properties();
+            InputStream in = DBConnection.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties");
 
+            properties.load(in);
+
+            url = properties.getProperty("db.url");
+            user = properties.getProperty("db.user");
+            password = properties.getProperty("db.password");
+
+            Class.forName(properties.getProperty("db.driver"));
+
+        } catch (Exception e) {
+            System.out.println("Database configuration error❌ ");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection()throws Exception{
+        return DriverManager.getConnection(url,user,password);
+    }
 }
