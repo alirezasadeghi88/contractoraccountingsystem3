ea1229cc83eabae1318eff3e1ea6a4386d20a391
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 00:21:07 -0800

    add contractoraccountingsystem3

---
+++ src/ir/university/accounting/util/DBConnection.java
@@ -0,0 +1,12 @@
+package ir.university.accounting.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DBConnection {
+    private static String url;
+    private static String user;
+    private static String password;
+
+
+}
