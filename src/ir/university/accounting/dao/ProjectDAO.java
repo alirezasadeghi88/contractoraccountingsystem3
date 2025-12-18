0db684f357fad856f223856dbd1af094dd2a60f1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-18 00:37:24 -0800

    add contractoraccountingsystem3

--- src/ir/university/accounting/dao/ProjectDAO.java
+++ src/ir/university/accounting/dao/ProjectDAO.java
@@ -3,5 +3,8 @@ package ir.university.accounting.dao;
 public class ProjectDAO {
     public void addProject(String name,String employer,String start,String end,double amount) {
 
+        String sql = "INSERT INTO PROJECTS" +
+                "(project_name, employer, start_date, end_date,contract_amount)" +
+                "VALUES (?, ?, ?, ?, ?)";
     }
 }
